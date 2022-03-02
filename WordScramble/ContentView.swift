c3ce2b72ed9aef644ba921fce428d287c6d20c99
Sadman Adib <sadmanadib67@gmail.com>
2022-03-03 00:07:08 +0600

    Fixed navigationTitle

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -33,8 +33,8 @@ struct ContentView: View {
                         
                     }
                 }
-            }
             }.navigationTitle(rootWord)
+            }
             .onSubmit {
                 addNewWord()
             }
