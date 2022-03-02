853cd06faeec9fd5da07221451ee32a4c9383ad6
Sadman Adib <sadmanadib67@gmail.com>
2022-03-02 23:34:03 +0600

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,68 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Sadman Adib on 3/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var newWord = ""
+    @State private var usedWords = [String] ()
+    @State private var rootWord = ""
+    
+    var body: some View {
+            NavigationView{
+            List{
+                Section{
+                    TextField("Enter your word", text:$newWord).autocapitalization(.none)
+                }
+                
+                Section{
+                    ForEach(usedWords, id:\.self){ word in
+                        HStack{
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                        
+                    }
+                }
+            }
+            }.navigationTitle(rootWord)
+            .onSubmit {
+                addNewWord()
+            }
+            .onAppear(perform: startGame)
+    }
+    
+    func startGame(){
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt"){
+            if let startWords = try? String(contentsOf: startWordsURL){
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+                
+                return
+            }
+        }
+        
+        fatalError("Could not load start.txt from bundle")
+    }
+    
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count > 0 else {return}
+        withAnimation{
+            usedWords.insert(answer, at:0)
+        }
+       
+        newWord=""
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
